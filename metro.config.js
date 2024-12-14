ea27d40e80b10791b5938bb542d6b9c147693d6b
mahmoudelsaadany <mahmoud.elsaadany@syngenta.com>
2024-12-14 18:06:54 +0200

    fix assets images problem

--- metro.config.js
+++ metro.config.js
@@ -1,21 +1,25 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
-module.exports = (async () => {
-    const {
-      resolver: { assetExts },
-    } = await getDefaultConfig();
-    return {
-      resolver: {
-        assetExts: [...assetExts, 'png'], // Add extensions for assets
+const {
+  resolver: { sourceExts, assetExts },
+} = getDefaultConfig(__dirname);
+
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
       },
-    };
-  })();
+    }),
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+  },
+};
+
+module.exports = mergeConfig(defaultConfig, config);
